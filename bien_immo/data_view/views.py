8e9a67e87bacbd5f400d69b2e43a63f3fc0a3942
phguerinea <philguercody@gmail.com>
2021-12-19 23:17:11 +0100

    Uplaod et affichage données

--- bien_immo/data_view/views.py
+++ bien_immo/data_view/views.py
@@ -20,11 +20,14 @@ def main(request):
 def read_immo():
     conn = psycopg2.connect("dbname=test user=postgres password=jaune2000")
     cur = conn.cursor()
-    data=pd.read_csv('static/immo_CSV.csv',delimiter=",")
+    data=pd.read_csv('static/immo_CSV.csv',delimiter=";")
     print("DAAAAAAAAATA")
     print(data.shape)
-    #data = data['prix_tva_reduite'].replace(to_replace='', value=0)
-    #print(data['prix_tva_reduite'])
+    print(data.columns)
+    print(type(data))
+    data['prix_tva_reduite'] = data['prix_tva_reduite'].fillna(0)
+    print(data)
+    print(data['prix_tva_reduite'])
     #data.to_sql("immoTable",conn)
     #cur.execute("DROP * FROM immo WHERE balcony=True")
     #db_table_nm="immo"
@@ -33,14 +36,14 @@ def read_immo():
     cur.execute(qry)
     #path ='D:/Bureau/Formation/brief14/bien_immo/static/immo_CSV.csv'
 
-    '''with open(csv_name , 'r') as f:
+    with open(csv_name , 'r') as f:
     # Notice that we don't need the `csv` module.
         next(f) # Skip the header row.
-        cur.copy_from(f, 'immo', sep=';',null="None")
+        cur.copy_from(f, 'immo', sep=';',null='')
     conn.commit()
     #path ='static/immo_CSV.csv'
     #qry = "COPY immo(Id, id_lot,nb_piece,typologie,prix_tva_reduite,prix_tva_normale,\"prix_HT\",\"prix_m2_HT\",\"prix_m2_TTC\",orientation,exterieur,balcony,garden,parking,ville,departement,date_fin_programme,adresse_entiere,date_extraction) FROM '"+path+"' DELIMITER ';' CSV HEADER;"
-    #cur.execute(qry)'''
+    #cur.execute(qry)
     sql= pd.read_sql_query("SELECT * FROM immo WHERE balcony = False ",conn)
     print(sql)
     return "<p>"+str(sql)+"<p><p><a href=\"../\">Lien vers load_csv</a>" 
